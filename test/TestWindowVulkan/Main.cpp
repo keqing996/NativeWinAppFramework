7a33c27fd8d8ed4f64759d5de100280cee3c312a
Noemie <epigrf@outlook.com>
2024-07-13 19:36:46 +0800

    Make vulkan test work.

--- test/TestWindowVulkan/Main.cpp
+++ test/TestWindowVulkan/Main.cpp
@@ -381,6 +381,7 @@ int main()
 #pragma region [Graphics pipeline]
 
     VkPipelineLayout pipelineLayout;
+    VkPipeline graphicsPipeline;
 
     {
         auto CreateShaderModule = [&logicDevice](const unsigned char* code, size_t size) -> VkShaderModule
@@ -477,6 +478,25 @@ int main()
         if (::vkCreatePipelineLayout(logicDevice, &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS)
             throw std::runtime_error("failed to create pipeline layout!");
 
+        VkGraphicsPipelineCreateInfo pipelineInfo {};
+        pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+        pipelineInfo.stageCount = 2;
+        pipelineInfo.pStages = shaderStages;
+        pipelineInfo.pVertexInputState = &vertexInputInfo;
+        pipelineInfo.pInputAssemblyState = &inputAssembly;
+        pipelineInfo.pViewportState = &viewportState;
+        pipelineInfo.pRasterizationState = &rasterizer;
+        pipelineInfo.pMultisampleState = &multisampling;
+        pipelineInfo.pColorBlendState = &colorBlending;
+        pipelineInfo.pDynamicState = &dynamicState;
+        pipelineInfo.layout = pipelineLayout;
+        pipelineInfo.renderPass = renderPass;
+        pipelineInfo.subpass = 0;
+        pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
+
+        if (vkCreateGraphicsPipelines(logicDevice, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &graphicsPipeline) != VK_SUCCESS)
+            throw std::runtime_error("failed to create graphics pipeline!");
+
         ::vkDestroyShaderModule(logicDevice, fragShaderModule, nullptr);
         ::vkDestroyShaderModule(logicDevice, vertShaderModule, nullptr);
     }
@@ -532,6 +552,33 @@ int main()
 
 #pragma endregion
 
+#pragma region [Sync objects]
+
+    VkSemaphore imageAvailableSemaphore;
+    VkSemaphore renderFinishedSemaphore;
+    VkFence inFlightFence;
+
+    {
+        VkFenceCreateInfo fenceInfo {};
+        fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+        fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+
+        VkSemaphoreCreateInfo semaphoreInfo {};
+        semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+
+        if (::vkCreateSemaphore(logicDevice, &semaphoreInfo, nullptr, &imageAvailableSemaphore) != VK_SUCCESS)
+            throw std::runtime_error("failed to create semaphore!");
+
+        if (::vkCreateSemaphore(logicDevice, &semaphoreInfo, nullptr, &renderFinishedSemaphore) != VK_SUCCESS)
+            throw std::runtime_error("failed to create semaphore!");
+
+        if (::vkCreateFence(logicDevice, &fenceInfo, nullptr, &inFlightFence) != VK_SUCCESS)
+            throw std::runtime_error("failed to create fence!");
+    }
+
+
+#pragma endregion
+
     // Main loop
     while (true)
     {
@@ -539,17 +586,106 @@ int main()
 
         if (std::ranges::any_of(window.PopAllEvent(), [](const NWA::WindowEvent& event) -> bool { return event.type == NWA::WindowEvent::Type::Close; }))
             break;
+
+        ::vkWaitForFences(logicDevice, 1, &inFlightFence, VK_TRUE, UINT64_MAX);
+        ::vkResetFences(logicDevice, 1, &inFlightFence);
+
+        uint32_t imageIndex;
+        ::vkAcquireNextImageKHR(logicDevice, swapchain, UINT64_MAX, imageAvailableSemaphore, VK_NULL_HANDLE, &imageIndex);
+
+        ::vkResetCommandBuffer(commandBuffer, /*VkCommandBufferResetFlagBits*/ 0);
+
+        // record command buffer
+        {
+            VkCommandBufferBeginInfo beginInfo{};
+            beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+            if (::vkBeginCommandBuffer(commandBuffer, &beginInfo) != VK_SUCCESS)
+                throw std::runtime_error("failed to begin recording command buffer!");
+
+            VkRenderPassBeginInfo renderPassInfo{};
+            renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+            renderPassInfo.renderPass = renderPass;
+            renderPassInfo.framebuffer = swapchainFramebuffers[imageIndex];
+            renderPassInfo.renderArea.offset = {0, 0};
+            renderPassInfo.renderArea.extent = swapchainExtent;
+
+            VkClearValue clearColor = {{{0.0f, 0.0f, 0.0f, 1.0f}}};
+            renderPassInfo.clearValueCount = 1;
+            renderPassInfo.pClearValues = &clearColor;
+
+            ::vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+            ::vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
+
+            VkViewport viewport{};
+            viewport.x = 0.0f;
+            viewport.y = 0.0f;
+            viewport.width = static_cast<float>(swapchainExtent.width);
+            viewport.height = static_cast<float>(swapchainExtent.height);
+            viewport.minDepth = 0.0f;
+            viewport.maxDepth = 1.0f;
+            ::vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+
+            VkRect2D scissor{};
+            scissor.offset = {0, 0};
+            scissor.extent = swapchainExtent;
+            ::vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
+
+            ::vkCmdDraw(commandBuffer, 3, 1, 0, 0);
+
+            ::vkCmdEndRenderPass(commandBuffer);
+
+            if (::vkEndCommandBuffer(commandBuffer) != VK_SUCCESS)
+                throw std::runtime_error("failed to record command buffer!");
+        }
+
+        VkSubmitInfo submitInfo{};
+        submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+
+        VkSemaphore waitSemaphores[] = { imageAvailableSemaphore };
+        VkPipelineStageFlags waitStages[] = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
+        submitInfo.waitSemaphoreCount = 1;
+        submitInfo.pWaitSemaphores = waitSemaphores;
+        submitInfo.pWaitDstStageMask = waitStages;
+        submitInfo.commandBufferCount = 1;
+        submitInfo.pCommandBuffers = &commandBuffer;
+
+        VkSemaphore signalSemaphores[] = { renderFinishedSemaphore };
+        submitInfo.signalSemaphoreCount = 1;
+        submitInfo.pSignalSemaphores = signalSemaphores;
+
+        if (::vkQueueSubmit(deviceQueue, 1, &submitInfo, inFlightFence) != VK_SUCCESS)
+            throw std::runtime_error("failed to submit draw command buffer!");
+
+        VkPresentInfoKHR presentInfo{};
+        presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+        presentInfo.waitSemaphoreCount = 1;
+        presentInfo.pWaitSemaphores = signalSemaphores;
+
+        VkSwapchainKHR swapChains[] = { swapchain };
+        presentInfo.swapchainCount = 1;
+        presentInfo.pSwapchains = swapChains;
+        presentInfo.pImageIndices = &imageIndex;
+
+        ::vkQueuePresentKHR(deviceQueue, &presentInfo);
     }
 
-    // Clearup
+    ::vkDeviceWaitIdle(logicDevice);
+
+    // Clean up
+
+    ::vkDestroySemaphore(logicDevice, imageAvailableSemaphore, nullptr);
+    ::vkDestroySemaphore(logicDevice, renderFinishedSemaphore, nullptr);
+    ::vkDestroyFence(logicDevice, inFlightFence, nullptr);
 
     ::vkDestroyCommandPool(logicDevice, commandPool, nullptr);
 
     for (auto framebuffer : swapchainFramebuffers)
         ::vkDestroyFramebuffer(logicDevice, framebuffer, nullptr);
 
+    ::vkDestroyPipeline(logicDevice, graphicsPipeline, nullptr);
     ::vkDestroyPipelineLayout(logicDevice, pipelineLayout, nullptr);
-
     ::vkDestroyRenderPass(logicDevice, renderPass, nullptr);
 
     for (auto imageView : swapchainImageViews)
